a25269604071cfc83e7a95dde8f4978bdb5bd5ef
Paulus Janssen <paulus.janssen@GEODAN.NL>
2017-07-10 17:32:23 +0200

    work

---
+++ Receive/Receive.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName= "rabbitAdmin", Password = "some_secret_password", VirtualHost="testHost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            var result = channel.QueueDeclare(exclusive:true);
+
+            //channel.QueueDeclare(queue: "testQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            //channel.QueueBind("testQueue", "amq.topic", "#");
+            channel.QueueBind(exchange:"dbMsg", queue:result.QueueName, routingKey:"#");
+    
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: result.QueueName, autoAck: true, consumer: consumer);            
+            //channel.BasicConsume(queue: "testQueue", autoAck: true, consumer: consumer);            
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+//  public static void Main()
+//     {
+//         var factory = new ConnectionFactory() { HostName = "localhost" };
+//         using(var connection = factory.CreateConnection())
+//         using(var channel = connection.CreateModel())
+//         {
+//             //channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+//             channel.QueueDeclare(queue: "testQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+//             var consumer = new EventingBasicConsumer(channel);
+//             consumer.Received += (model, ea) =>
+//             {
+//                 var body = ea.Body;
+//                 var message = Encoding.UTF8.GetString(body);
+//                 Console.WriteLine(" [x] Received {0}", message);
+//             };
+//             channel.BasicConsume(queue: "testQueue", autoAck: true, consumer: consumer);
+//             //channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+//             Console.WriteLine(" Press [enter] to exit.");
+//             Console.ReadLine();
+//         }
+//     }
---
+++ Send/Send.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost", UserName= "rabbitAdmin", Password = "some_secret_password", VirtualHost="testHost" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				// channel.QueueDeclare(queue: "hello",
+				// 					 durable: false,
+				// 					 exclusive: false,
+				// 					 autoDelete: false,
+				// 					 arguments: null);
+				 channel.QueueDeclare(queue: "testQueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind("testQueue", "amq.topic", "#");
+
+				string message = "Hello World! pj";
+				var body = Encoding.UTF8.GetBytes(message);
+
+				channel.BasicPublish(exchange: "amq.topic",
+									 routingKey: "#",
+									 basicProperties: null,
+									 body: body);
+				Console.WriteLine(" [x] Sent {0}", message);
+			}
+
+			Console.WriteLine(" Press [enter] to exit.");
+			Console.ReadLine();
+		}
+	}
+}
